5c13520bfa2c81b01c96f294ceacf2d72fe22f13
Peter Vrabel <kybu@kybu.org>
2011-11-24 16:41:02 -0500

    Nested messages are correctly converted to a hash

--- lib/beefcake.rb
+++ lib/beefcake.rb
@@ -270,9 +270,18 @@ module Beefcake
 
     def to_hash
       __beefcake_fields__.values.inject({}) do |h, fld|
-        value = self[fld.name]
-        unless value.nil?
-          h[fld.name] = value
+        if v = self[fld.name]
+          h[fld.name] =
+              case
+                # A nested protobuf message, so let's call its 'to_hash' method.
+                when v.respond_to?(:to_hash) ; v.to_hash
+                when v.is_a?(Array)
+                  # There can be two field types stored in array.
+                  # Primitive type or nested another protobuf message.
+                  # The later one has got a 'to_hash' method.
+                  h[fld.name] = v.map { |i| i.respond_to?(:to_hash) ? i.to_hash : i }
+                else ; v
+              end
         end
         h
       end
